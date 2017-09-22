fbbfe731608e21e133c0c8ad7451d2f66bb75189
miguelsaenzbrand <saenzbrand@gmail.com>
2017-09-22 10:00:34 -0300

    mapa en contact y sponsors individuales

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -26,11 +26,4 @@
     offset: 54
   });
 
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
-
 })(jQuery); // End of use strict
